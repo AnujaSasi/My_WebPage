dda6e51d920a97dda83e2785fdfbb4a5b424ddf8
AnujaSasi <98422825+AnujaSasi@users.noreply.github.com>
2023-06-19 11:06:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeColor() {
+    var textElement = document.getElementById('text');
+    textElement.style.color = getRandomColor();
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
